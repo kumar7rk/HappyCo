5cfbff7ab5e46c748dc925a0c264ae63b412fca4
Rohit Kumar <rohit@happy.co>
2018-03-15 13:58:28 +1030

    Removing commented code :no_entry:
    inspection query v1.0

--- YumiBotGo/code.go
+++ YumiBotGo/code.go
@@ -137,12 +137,8 @@ func getUserData(u_id string,){
 	if err != nil {
 		fmt.Println("Error connecting");
 	}
- /*
- SELECT folders.business,folders.user,folders.role ,folders.folder_id,folders.folder_name,i.created_at::date as created_at,i.template_name,i.id,i.status,i.location FROM (SELECT businesses.business_id as business,businesses.user_id as user,role_id as role,folder_id as folder_id,folder_name as folder_name FROM (SELECT bm.business_id as business_id,bm.user_id as user_id,bm.business_role_id as role_id,f.id as folder_id,f.name as folder_name FROM business_membership as bm JOIN portfolios as f ON bm.business_id = f.business_id WHERE bm.user_id = 22772 AND bm.inactivated_at IS NULL AND f.inactivated_at IS NULL) as businesses GROUP BY businesses.business_id,businesses.role_id,businesses.user_id,folder_id,folder_name ORDER BY businesses.business_id ) as folders JOIN inspections as i ON folders.folder_id = i.folder_id WHERE i.user_id = '22772' AND i.archived_at IS NULL ORDER BY i.created_at DESC LIMIT 5;
-    */
 	//running the query on the db
 	// right now it's fetching last 5 inspection for user_id 63135
-//	rows, err := db.Query("SELECT id, folder_id, created_at, template_name FROM inspections WHERE user_id = $1 AND FOLDER_ID IN ( SELECT portfolio_id FROM portfolio_access_controls WHERE user_id = $3) AND archived_at IS NULL ORDER BY created_at DESC LIMIT $2", u_id,5,u_id)
 	rows, err := db.Query("SELECT folders.business,folders.user,folders.role ,folders.folder_id,folders.folder_name,i.created_at::date as created_at,i.template_name,i.id,i.status,i.location FROM (SELECT businesses.business_id as business,businesses.user_id as user,role_id as role,folder_id as folder_id,folder_name as folder_name FROM (SELECT bm.business_id as business_id,bm.user_id as user_id,bm.business_role_id as role_id,f.id as folder_id,f.name as folder_name FROM business_membership as bm JOIN portfolios as f ON bm.business_id = f.business_id WHERE bm.user_id = $1 AND bm.inactivated_at IS NULL AND f.inactivated_at IS NULL) as businesses GROUP BY businesses.business_id,businesses.role_id,businesses.user_id,folder_id,folder_name ORDER BY businesses.business_id ) as folders JOIN inspections as i ON folders.folder_id = i.folder_id WHERE i.user_id = $3 AND i.archived_at IS NULL ORDER BY i.created_at DESC LIMIT $2",u_id,5,u_id);
 	if err != nil {
     	panic(err)
