3db990ffc41dd22a80a827d06900ec3fca51f0f7
Rohit Kumar <rohit@happy.co>
2018-07-26 12:08:47 +0930

    Connecting to DB in main :rocket:
    was connecting in getUserData
    making db global variable to use in the db queries in getUserData

--- YumiBotGo/code.go
+++ YumiBotGo/code.go
@@ -88,6 +88,8 @@ var integrationRec []Integration
 
 var planRec []Plan
 
+var db *sqlx.DB
+
 // structs for reading payload in json received from Intercom
 type User struct {
 	UserID string `json:"user_id"`
@@ -114,6 +116,20 @@ func main() {
 	err := loadEnv()
 	if err != nil {
 		fmt.Println("Error loading .env file")
+	}
+
+	postgresURI := formURI()
+	if postgresURI == "" {
+		fmt.Println("URI error")
+	}
+
+	db, err = connect(postgresURI)
+
+	if err == nil {
+		fmt.Println("DB Connected")
+	}
+	if err != nil {
+		fmt.Println("Error connecting")
 	}
 
 	//handling every new convesations in newConversation method
@@ -266,23 +282,9 @@ func connect(postgresURI string) (*sqlx.DB, error) {
 //queries the db and adds returned values in array
 func getUserData(u_id string) {
 
-	postgresURI := formURI()
-	if postgresURI == "" {
-		fmt.Println("URI error")
-	}
-
-	db, err := connect(postgresURI)
-
-	if err == nil {
-		fmt.Println("DB Connected")
-	}
-	if err != nil {
-		fmt.Println("Error connecting")
-	}
-
 	//fetching most recent (5) inspections for the user within the last 30 days.
 
-	err = db.Select(&inspectionsRec, "SELECT folders.business,folders.user,folders.role ,folders.folder_id,folders.folder_name,i.created_at as created_at,i.template_name,i.id,i.status,i.location FROM (SELECT businesses.business_id as business,businesses.user_id as user,role_id as role,folder_id as folder_id,folder_name as folder_name FROM (SELECT bm.business_id as business_id,bm.user_id as user_id,bm.business_role_id as role_id,f.id as folder_id,f.name as folder_name FROM business_membership as bm JOIN portfolios as f ON bm.business_id = f.business_id WHERE bm.user_id = $1 AND bm.inactivated_at IS NULL AND f.inactivated_at IS NULL) as businesses GROUP BY businesses.business_id,businesses.role_id,businesses.user_id,folder_id,folder_name ORDER BY businesses.business_id ) as folders JOIN inspections as i ON folders.folder_id = i.folder_id WHERE i.user_id = $1::varchar AND i.archived_at IS NULL AND i.created_at > (CURRENT_DATE- interval '30 day') ORDER BY i.created_at DESC LIMIT $2", u_id, 5)
+	err := db.Select(&inspectionsRec, "SELECT folders.business,folders.user,folders.role ,folders.folder_id,folders.folder_name,i.created_at as created_at,i.template_name,i.id,i.status,i.location FROM (SELECT businesses.business_id as business,businesses.user_id as user,role_id as role,folder_id as folder_id,folder_name as folder_name FROM (SELECT bm.business_id as business_id,bm.user_id as user_id,bm.business_role_id as role_id,f.id as folder_id,f.name as folder_name FROM business_membership as bm JOIN portfolios as f ON bm.business_id = f.business_id WHERE bm.user_id = $1 AND bm.inactivated_at IS NULL AND f.inactivated_at IS NULL) as businesses GROUP BY businesses.business_id,businesses.role_id,businesses.user_id,folder_id,folder_name ORDER BY businesses.business_id ) as folders JOIN inspections as i ON folders.folder_id = i.folder_id WHERE i.user_id = $1::varchar AND i.archived_at IS NULL AND i.created_at > (CURRENT_DATE- interval '30 day') ORDER BY i.created_at DESC LIMIT $2", u_id, 5)
 	if err != nil {
 	   fmt.Fprintf(os.Stderr, "Error in inspection query %v: %v\n", u_id, err)
 	   	return
