23d3331f71ca7e600bc5c6be04be34cd29652f06
Rohit Kumar <rohit@happy.co>
2018-08-15 13:41:48 +0930

    Tweaking password message :passport_control:

--- YumiBotGo/password.go
+++ YumiBotGo/password.go
@@ -20,7 +20,7 @@ func sendPasswordReply(user User, conversationID string, params... string){
 	if len(params) > 0 {
 		name = params[0]
 	}
-	message := "Hi " + name + " 👋 \n \n You can reset your password either through the web application or iOS application by clicking Forgot Your Password. \n\nAlso be sure to check out our FAQ on managing your profile: \n\n <a href='http://intercom.help/happyco/frequently-asked-questions/happy-inspector-faq-setup-and-user-management/faq-managing-passwords-user-details-and-access-to-your-properties'> FAQ: Managing Passwords, User Details and Access to Your Properties</a> \n\n Let me know if you\n\n still have issues logging in, \n Thanks!  \n HappyCo team ☺"		
+	message := "Hi " + name + " 👋 \n \n It looks like you might be having trouble logging in? \n\n You can reset your password either through the web application or iOS application by clicking Forgot Your Password. \n\nAlso be sure to check out our FAQ on managing your profile: \n\n <a href='http://intercom.help/happyco/frequently-asked-questions/happy-inspector-faq-setup-and-user-management/faq-managing-passwords-user-details-and-access-to-your-properties'> FAQ: Managing Passwords, User Details and Access to Your Properties</a> \n\n Let me know if you still have issues logging in, \n Thanks!  \n YumiBot ☺ \n\n(Need to speak with one of our team?- please reply and we’ll get back to you as soon as we can)”"		
 	_, err := ic.Conversations.Reply(conversationID, intercom.Admin{ID: "207278"}, intercom.CONVERSATION_COMMENT, message)
 
 		if herr, ok := err.(intercom.IntercomError); ok && herr.GetCode() == "not_found" {
