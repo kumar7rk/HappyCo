5a0a64cf21f324b0054a8fe13688eed0186858a5
Rohit Kumar <rohit@happy.co>
2018-08-16 15:28:05 +0930

    Getting information for a person having multiple position in his current company and single position in current company :godmode:

--- Froso/linkedin/linkedIn.js
+++ Froso/linkedin/linkedIn.js
@@ -2,65 +2,121 @@ const puppeteer = require('puppeteer');
 const CREDS = require('./creds');
 
 (async () => {
-	const browser = await puppeteer.launch({
-		headless: false
-	});
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+  /*const browser = await puppeteer.launch({
+        ignoreHTTPSErrors: true
+    });*/
+  const page = await browser.newPage();
 
-	const page = await browser.newPage();
-	
-	await page.goto("https://www.linkedin.com");
+  await page.goto("https://www.linkedin.com");
 
-	const USERNAME_SELECTOR = '#login-email';
-  	const PASSWORD_SELECTOR = '#login-password';
-  	const BUTTON_SELECTOR = '#login-submit';
+  const USERNAME_SELECTOR = '#login-email';
+  const PASSWORD_SELECTOR = '#login-password';
+  const BUTTON_SELECTOR = '#login-submit';
 
-  	await page.click(USERNAME_SELECTOR);
-  	await page.keyboard.type(CREDS.username);
+  await page.click(USERNAME_SELECTOR);
+  await page.keyboard.type(CREDS.username);
 
-  	await page.click(PASSWORD_SELECTOR);
-  	await page.keyboard.type(CREDS.password);
-	
-	await page.click(BUTTON_SELECTOR);
-    await page.waitForNavigation();
-  	
-	var url = "https://www.linkedin.com/in/robert-wise/";
+  await page.click(PASSWORD_SELECTOR);
+  await page.keyboard.type(CREDS.password);
+  
+  await page.click(BUTTON_SELECTOR);
+  await page.waitForNavigation();
+    
+if(typeof require !== 'undefined') XLSX = require('xlsx');
 
-	await page.goto(url);
-	
-	await page.waitFor(2 * 1000);
-	await page.evaluate(_ => {
-	  window.scrollBy(0, window.innerHeight);
-	});
-	await page.waitFor(2 * 1000);
-   
-   const title = await page.evaluate(() => document.querySelector
-   	('div.pv-entity__summary-info.pv-entity__summary-info--v2 >h3').textContent)
-   
-   console.log(title);
-   
-   const title1 = await page.evaluate(() => document.querySelector
-   	('#experience-section > ul > div > li > a > div > h4').textContent)
-  /* ul
-	div
-	li
-	a
-	div
-	h3*/
-   console.log(title1);
-   const title2 = await page.evaluate(() => document.querySelector
-   	('div > h4 > span:nth-child(2)').textContent)
-   console.log(title2);
-   
-   //#ember6250 > div.pv-entity__summary-info.pv-entity__summary-info--v2 > h4.Sans-17px-black-85\25 > span.pv-entity__secondary-title
-   //#ember6250 > div.pv-entity__summary-info.pv-entity__summary-info--v2 > h4.pv-entity__date-range.inline-block.Sans-15px-black-70\25 > span:nth-child(2)
-   
-   const title3 = await page.evaluate(() => document.querySelector
-   	('div > h4.pv-entity__date-range.inline-block > span:nth-child(2)').textContent)
-   console.log(title3);
-   //#ember6250 > div.pv-entity__summary-info.pv-entity__summary-info--v2 > h4:nth-child(4) > span.pv-entity__bullet-item-v2
+  var workbook = XLSX.readFile('LinkedIn.xlsx');
+  var first_sheet_name = workbook.SheetNames[0];
+  var worksheet = workbook.Sheets[first_sheet_name];
+    var address_of_cell = 'A2';
+    var desired_cell = worksheet[address_of_cell];
+  
+  for (var i = 2; i < 4; i++) {
+    address_of_cell = 'A'+i;
+    desired_cell = worksheet[address_of_cell];
+    var desired_value = (desired_cell ? desired_cell.v : undefined);
 
-  const title4 = await page.evaluate(() => document.querySelector
-   	('div > h4:nth-child(4)	 > span.pv-entity__bullet-item-v2').textContent)
-   console.log(title4);
- await browser.close();
+  await page.goto(desired_value); 
+  
+  await page.waitFor(2 * 1000);
+  await page.evaluate(_ => {
+    window.scrollBy(0, window.innerHeight);
+  });
+
+  await page.waitFor(2 * 1000);
+
+  //contains title
+  var data = await page.evaluate(() => document.querySelector
+    ('div.pv-entity__summary-info.pv-entity__summary-info--v2 >h3').textContent)
+  console.log(data);
+  var multiJob = data.includes("Title");
+  if (!multiJob) {
+    console.log("It's Rob")
+    var name = await page.evaluate(() => document.querySelector
+      ('div.pv-top-card-v2-section__info.mr5 > div.display-flex.align-items-center > h1').textContent)
+        writeCell = 'B'+i
+        worksheet[writeCell].v = name.trim();
+    
+    var companyName = await page.evaluate(() => document.querySelector
+    ('div > h4 > span:nth-child(2)').textContent)
+        writeCell = 'C'+i
+        worksheet[writeCell].v = companyName;
+
+    var title = await page.evaluate(() => document.querySelector
+    ('div.pv-entity__summary-info.pv-entity__summary-info--v2 >h3').textContent)
+        writeCell = 'D'+i
+        worksheet[writeCell].v = title;
+
+    var currentJobDuration = await page.evaluate(() => document.querySelector
+      ('div > h4:nth-child(4) > span.pv-entity__bullet-item-v2').textContent)
+
+    writeCell = 'E'+i
+        worksheet[writeCell].v = currentJobDuration;
+  }
+  else{
+      console.log("It's Dheer")
+      var name = await page.evaluate(() => document.querySelector
+        ('div.pv-top-card-v2-section__info.mr5 > div.display-flex.align-items-center > h1').textContent)
+          writeCell = 'B'+i
+          worksheet[writeCell].v = name.trim();
+      
+      var companyName = await page.evaluate(() => document.querySelector
+      ('div > h3 > span:nth-child(2)').textContent)
+          writeCell = 'C'+i
+          worksheet[writeCell].v = companyName;
+
+      var title = await page.evaluate(() => document.querySelector
+      ('div.pv-entity__summary-info.pv-entity__summary-info--v2 >h3').textContent)
+          title = title.replace('Title','').trim();
+          writeCell = 'D'+i
+          worksheet[writeCell].v = title;
+
+      var currentJobDuration = await page.evaluate(() => document.querySelector
+        ('div > h4:nth-child(3) > span.pv-entity__bullet-item-v2').textContent)
+
+      writeCell = 'E'+i
+          worksheet[writeCell].v = currentJobDuration;
+      }
+  
+//Main which is title for rob and company name for Dheer
+  
+//Rob- 
+//Dheer#ember5440 > div > div.pv-entity__company-summary-info > h3 > span:nth-child(2)
+
+        //Rob- #ember4297 > div.pv-entity__summary-info.pv-entity__summary-info--v2 > h3
+        // Dheer- #ember4535 > div > div > div.pv-entity__summary-info.pv-entity__summary-info--v2.pv-entity__summary-info-margin-top > h3 > span:nth-child(2)
+
+
+//#ember4535 > div > div > div.pv-entity__summary-info.pv-entity__summary-info--v2.pv-entity__summary-info-margin-top > h4:nth-child(3)
+    
+
+
+    //Rob- #ember2002 > div.pv-entity__summary-info.pv-entity__summary-info--v2 > h4:nth-child(4) > span.pv-entity__bullet-item-v2
+    //Dheer#ember4535 > div > div > div.pv-entity__summary-info.pv-entity__summary-info--v2.pv-entity__summary-info-margin-top > h4:nth-child(3) > span.pv-entity__bullet-item-v2
+    
+      XLSX.writeFile(workbook ,'LinkedIn2.xlsx')
+  }
+  await browser.close();
 })();
