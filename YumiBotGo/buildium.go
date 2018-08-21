0d0f3ed6dc5c7b1655f4bc167a21cfc066838385
Rohit Kumar <rohit@happy.co>
2018-08-21 14:37:23 +0930

    Refactoring

--- YumiBotGo/buildium.go
+++ YumiBotGo/buildium.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"strings"
+	// intercom "gopkg.in/intercom/intercom-go.v2"
 )
 
 //********************************************Sending Buildium reply********************************************
@@ -18,6 +19,6 @@ func sendBuildiumReply(user User, conversationID string, params ...string) {
 	message := "Hi " + name + " 👋 \n \n <b>Our friends at Buildium support your Happy Inspector subscription. \n\n They can be reached at 888-414-1988, or by submitting a ticket through your Buildium account.</b>\n\nBuildium Support team are the best place to help you with this query as they understand your unique workflow and are trained in Happy Inspector 💫 \n \n Please also feel free to take a look through our FAQ on the Buildium integration:  \n https://intercom.help/happyco/frequently-asked-questions/buildium-integration-faq/faq-buildium-integration  \n Thanks!  \n HappyBot ☺"
 
 	addReply(conversationID, message)
-	// _, err = ic.Conversations.Assign(conversationID, intercom.Admin{ID: "207278"}, intercom.Admin{ID: "1615207"}) giving an error *Intercom.admin type...
+	// _, err := ic.Conversations.Assign(conversationID, intercom.Admin{ID: "207278"}, intercom.Admin{ID: "1615207"}) //giving an error *Intercom.admin type...
 
 }
