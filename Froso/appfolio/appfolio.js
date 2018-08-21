f98fa312f7f212c6f60eb2bd4bc4074d0155ca93
Rohit Kumar <rohit@happy.co>
2018-08-21 13:01:22 +0930

    Removed commented code and other improvements in the code :crocodile:

--- Froso/appfolio/appfolio.js
+++ Froso/appfolio/appfolio.js
@@ -9,20 +9,6 @@
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  var url =  ["https://vequity.appfolio.com/connect/users/sign_in","https://benchmarkmfr.appfolio.com/connect/users/sign_in","https://realprop.appfolio.com/connect/users/sign_in"
-             ,"https://ottosen.appfolio.com/connect/users/sign_in","https://sunpropertymanagement.appfolio.com/connect/users/sign_in"];
-/*
-  for (var i = 0; i < url.length; i++) {
-    await page.goto(url[i]);
-
-    const name = await page.evaluate(() => document.querySelector('.footer__info > p').innerText)
-    const phone = await page.evaluate(() => document.querySelector('.footer__info > a').innerText)
-    const website = await page.evaluate(() => document.querySelector('.footer__info > p > a ').href)
-    
-    console.log(name+ "\n" + phone+ "\n" + website)   
-  }
-*/
-
 if(typeof require !== 'undefined') XLSX = require('xlsx');
 
 
@@ -32,74 +18,45 @@ if(typeof require !== 'undefined') XLSX = require('xlsx');
   
   var address_of_cell = 'A2';
   var desired_cell = worksheet[address_of_cell];
-  var desired_value = (desired_cell ? desired_cell.v : undefined);
-//  console.log(desired_value)
-  
-// worksheet['B2'].v = 'manual';
-//  XLSX.writeFile(workbook ,'Appfolio2.xlsx')
+  var url = (desired_cell ? desired_cell.v : undefined);
 
   var t0 = performance.now();
-  
-  for (var i = 2; i < 246; i++) {
+
+  for (var i = 2; i < 4; i++) {
     address_of_cell = 'A'+i
-    var desired_cell = worksheet[address_of_cell];
-    var desired_value = (desired_cell ? desired_cell.v : undefined);
+    desired_cell = worksheet[address_of_cell];
+    url = (desired_cell ? desired_cell.v : undefined);
     
-    await page.goto(desired_value);
+    await page.goto(url);
 
     if (await page.evaluate(() => document.querySelector('.footer__info > p')) != null) {
       
       const name = await page.evaluate(() => document.querySelector('.footer__info > p').innerText)
       const phone = await page.evaluate(() => document.querySelector('.footer__info > a').innerText)
       const website = await page.evaluate(() => document.querySelector('.footer__info > p > a ').href)
+      
       var writeCell = 'B'+i
       worksheet[writeCell].v = name;   
 
-      var writeCell1 = 'C'+i
-      worksheet[writeCell1].v = website;   
+      writeCell = 'C'+i
+      worksheet[writeCell].v = website;   
 
-      //console.log(name+ "\n" + phone+ "\n" + website)   
+      console.log(name+ "\n" + phone+ "\n" + website+ "\n")   
     }
   }
-    XLSX.writeFile(workbook ,'Appfolio4.xlsx')
+    XLSX.writeFile(workbook ,'Appfolio1.xlsx')
 
   var t1 = performance.now();
-  console.log((t1-t0)/1000+ " seconds");
+  // console.log((t1-t0)/1000+ " seconds");
 
-  /*var range = {s: {c:0, r:1}, e: {c:0, r:245 }};
-  var column = 0;
+  var range = XLSX.utils.decode_range(worksheet['!ref']); // get the range
+  console.log(range)
   for (var R = range.s.r; R < range.e.R; ++R) {
-    var cellAddress = {c:column, r:R};
-    XLSX.utils.encode_cell(cellAddress);
-  }*/
-
-    /*var cell_address = 'B2'
-    for (var j = 2; j < 12; j++) {
-      cell_address = 'B'+j
-      var cell_desired = worksheet[cell_address]; 
-      XLSX.write(workbook ,name)
-    }*/
-  
-
- /* var range = {s: {c:0, r:1}, e: {c:0, r:245 }};
-for(var R = range.s.r; R <= range.e.r; ++R) {
-  for(var C = range.s.c; C <= range.e.c; ++C) {
-    var cell_address = {c:C, r:R};
-    /* if an A1-style address is needed, encode the address 
-    var cell_ref = XLSX.utils.encode_cell(cell_address);
-    console.log(cell_address)
+      console.log("Stuff");
+    for(var C = range.s.c; C <= range.e.c; ++C) {
+      console.log("More Stuff");
+    }
   }
-}*/
-
-/*
-  await page.goto('https://vequity.appfolio.com/connect/users/sign_in');
-  
-  const name = await page.evaluate(() => document.querySelector('.footer__info > p').innerText)
-  const phone = await page.evaluate(() => document.querySelector('.footer__info > a').innerText)
-  const website = await page.evaluate(() => document.querySelector('.footer__info > p > a ').href)
-	
-	//console.log(name+ "\n" + phone+ "\n" + website)
-*/
 
   await browser.close();
 })();
