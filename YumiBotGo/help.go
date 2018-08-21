1a798cd8034529baa5e6f970cbc09bb3a887c147
Rohit Kumar <rohit@happy.co>
2018-08-21 12:55:28 +0930

    Adding help.go

---
+++ YumiBotGo/help.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	intercom "gopkg.in/intercom/intercom-go.v2"
+	"os"
+	"strings"
+)
+//********************************************Sending Buildium reply********************************************
+func listRunCommands(author string, conversationID string, params ...string) {
+	accessToken := os.Getenv("INTERCOM_ACCESS_TOKEN")
+	ic := intercom.NewClient(accessToken, "")
+
+	name :=strings.Fields(author)
+	
+	message := "Yo " + name[0] + "\n \n <b>Try following commands</b> \n\n yumi run buildium \n\n yumi run password"
+
+	_, err := ic.Conversations.Reply(conversationID, intercom.Admin{ID: "207278"}, intercom.CONVERSATION_NOTE, message)
+	
+	if herr, ok := err.(intercom.IntercomError); ok && herr.GetCode() == "not_found" {
+		fmt.Fprintf(os.Stderr, "Error from Intercom when replying to Buildium %v: %v\n", "", err)
+		return
+	}
+
+}
