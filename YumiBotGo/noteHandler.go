c28e565aee2970eb5e881b21c63a7a7a4561539e
Rohit Kumar <rohit@happy.co>
2018-08-21 14:30:34 +0930

    Moving struct to intercom.go

--- YumiBotGo/noteHandler.go
+++ YumiBotGo/noteHandler.go
@@ -8,18 +8,6 @@ import (
 	"net/http"
 )
 
-type Author struct{
-	Name string `json:"name"`
-}
-type Part struct {
-	Body string `json:"body"`
-	Author Author `json:"author"`
-}
-
-type ConversationPart struct {
-	Part []Part `json:"conversation_parts"`
-}
-
 func newAdminNote(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("newAdminNote")
 	// Read body/payload
@@ -41,18 +29,14 @@ func newAdminNote(w http.ResponseWriter, r *http.Request) {
 	/* getting attributes from the received json
 	user- user's attributes - name id email type
 	conversationId - Intercom conversation ID
-	conversationmessage- user's message
+	note- team's message
+	author- team member who wrote the note
 	*/
 
 	user := msg.Data.Item.User
 	conversationId := msg.Data.Item.ConversationID
 	note := msg.Data.Item.ConversationPart.Part[0].Body
-	author := msg.Data.Item.ConversationPart.Part[0].Author.Name
-
-	fmt.Println(user)
-	fmt.Println(note)
-	fmt.Println(author)
-
+	author := msg.Data.Item.ConversationPart.Part[0].Author.Name	
 
 	go processNewAdminNote(user, conversationId, note, author)
 	w.WriteHeader(http.StatusOK)
@@ -60,16 +44,11 @@ func newAdminNote(w http.ResponseWriter, r *http.Request) {
 }
 
 func processNewAdminNote(user User, conversationID string, note string, author string) {
-	fmt.Println("processNewAdminNote")
-
 	if note == "<p>yumi run note</p>" {
 		makeAndSendNote(user, conversationID)
 	} else if note == "<p>yumi run buildium</p>" {
 		sendBuildiumReply(user, conversationID)
 	} else if note == "<p>yumi run password</p>" {
 		sendPasswordReply(user, conversationID)
-	} else if note == "<p>yumi help</p>" || note == "<p>yumi run help</p>"{
-		listRunCommands(author,conversationID)
-
 	}
 }
