6f8ea4c584821627005cf7ac12167184df751811
Rohit Kumar <rohit@happy.co>
2018-08-21 14:29:22 +0930

    Buildium:removing intercom reply and newClient code and putting in intercom.go :pray:

--- YumiBotGo/buildium.go
+++ YumiBotGo/buildium.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"fmt"
-	intercom "gopkg.in/intercom/intercom-go.v2"
-	"os"
 	"strings"
 )
 
 //********************************************Sending Buildium reply********************************************
 func sendBuildiumReply(user User, conversationID string, params ...string) {
-	accessToken := os.Getenv("INTERCOM_ACCESS_TOKEN")
-	ic := intercom.NewClient(accessToken, "")
 	// if you don't give a name use the name user have in his account
 
 	if len(params) == 0 {
@@ -22,12 +17,7 @@ func sendBuildiumReply(user User, conversationID string, params ...string) {
 	}
 	message := "Hi " + name + " 👋 \n \n <b>Our friends at Buildium support your Happy Inspector subscription. \n\n They can be reached at 888-414-1988, or by submitting a ticket through your Buildium account.</b>\n\nBuildium Support team are the best place to help you with this query as they understand your unique workflow and are trained in Happy Inspector 💫 \n \n Please also feel free to take a look through our FAQ on the Buildium integration:  \n https://intercom.help/happyco/frequently-asked-questions/buildium-integration-faq/faq-buildium-integration  \n Thanks!  \n HappyBot ☺"
 
-	_, err := ic.Conversations.Reply(conversationID, intercom.Admin{ID: "207278"}, intercom.CONVERSATION_COMMENT, message)
+	addReply(conversationID, message)
 	// _, err = ic.Conversations.Assign(conversationID, intercom.Admin{ID: "207278"}, intercom.Admin{ID: "1615207"}) giving an error *Intercom.admin type...
-
-	if herr, ok := err.(intercom.IntercomError); ok && herr.GetCode() == "not_found" {
-		fmt.Fprintf(os.Stderr, "Error from Intercom when replying to Buildium %v: %v\n", "", err)
-		return
-	}
 
 }
