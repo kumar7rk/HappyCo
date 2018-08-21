190aac73c84fe90a2a42dffba7ddb877f0862fa3
Rohit Kumar <rohit@happy.co>
2018-08-21 14:18:06 +0930

    Comments

--- YumiBotGo/help.go
+++ YumiBotGo/help.go
@@ -6,11 +6,9 @@ import (
 	"os"
 	"strings"
 )
-//********************************************Sending Buildium reply********************************************
+//********************************************Adding commands********************************************
 func listRunCommands(author string, conversationID string, params ...string) {
-	accessToken := os.Getenv("INTERCOM_ACCESS_TOKEN")
-	ic := intercom.NewClient(accessToken, "")
-
+	
 	name :=strings.Fields(author)
 	
 	message := "Yo " + name[0] + "\n \n <b>Try following commands</b> \n\n yumi run buildium \n\n yumi run password"
@@ -18,7 +16,7 @@ func listRunCommands(author string, conversationID string, params ...string) {
 	_, err := ic.Conversations.Reply(conversationID, intercom.Admin{ID: "207278"}, intercom.CONVERSATION_NOTE, message)
 	
 	if herr, ok := err.(intercom.IntercomError); ok && herr.GetCode() == "not_found" {
-		fmt.Fprintf(os.Stderr, "Error from Intercom when replying to Buildium %v: %v\n", "", err)
+		fmt.Fprintf(os.Stderr, "Error from Intercom when running yumi help%v: %v\n", "", err)
 		return
 	}
 
