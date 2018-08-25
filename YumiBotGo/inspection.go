b72dc626917f7799570742f8d26b5bb8382067aa
Rohit Kumar <rohit@happy.co>
2018-08-25 15:11:31 +0930

    Adding limit params inspection

--- YumiBotGo/inspection.go
+++ YumiBotGo/inspection.go
@@ -1,23 +1,25 @@
 package main
 
 import (
-	// "strings"
 	"time"
-	//"fmt"
+	"strconv"
 )
 func init() {
 	commands["inspection"] = showRecentInspections
 }
 //********************************************Adding commands********************************************
 func showRecentInspections(user User, conversationID string, params ...string) {
-
-	inspectionsRec := getInspections(user.UserID)
-	message := "<b>Showing recent inspections (max 5) created by "+ user.Name+" in last 30 days</b>"
+	var limit = 5
+	if len(params) > 0 {
+		limit, _ = strconv.Atoi(params[0])	
+	}
+	inspectionsRec := getInspections(user.UserID, limit)
+	message := "<b>Showing "+ strconv.Itoa(limit)+" recent inspections created by "+ user.Name+" in last 30 days</b>"
 	message += "\n"
 	for _, inspection := range inspectionsRec {
 		var url = "https://manage.happyco.com/folder/" + inspection.FolderID + "/inspections/" + inspection.ID
 		var date, _ = time.Parse(time.RFC3339, inspection.CreatedAt)
-		formattedDate := date.Format("02 Jan 2006 3:04PM")
+		var formattedDate = date.Format("02 Jan 2006 3:04PM")
 
 		message += "<a href=" + url + ">" + url + "</a>" + " " + formattedDate
 		message += "\n"
