5b6564483bd177598b86ca2bf205c302d2184727
Rohit Kumar <rohit@happy.co>
2018-08-25 12:48:58 +0930

    Formatting help query message :herb:

--- YumiBotGo/help.go
+++ YumiBotGo/help.go
@@ -9,10 +9,11 @@ func listRunCommands(author string, conversationID string, params ...string) {
 
 	name := strings.Fields(author)
 
-	message := "Yo " + name[0] + "\n \n <b>Try following commands</b>"
+	message := "Yo! " + name[0] + "\n \n <b>Try following commands</b>"
 	for cmd, _ := range commands {
 		message += "\n\n yumi run " + cmd
 	}
+	message += "\n"
 	message += "<a href=" + "https://hpy.io/yumi" + ">" + "Feedback/Report incorrect information" + "</a>"
 	addNote(conversationID, message)
 }
