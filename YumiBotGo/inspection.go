b5d908fac421d4ae0784b33314f1af10a01574bb
Rohit Kumar <rohit@happy.co>
2018-08-25 14:31:21 +0930

    Refactoring

--- YumiBotGo/inspection.go
+++ YumiBotGo/inspection.go
@@ -12,11 +12,10 @@ func init() {
 func showRecentInspections(user User, conversationID string, params ...string) {
 
 	inspectionsRec := getInspections(user.UserID)
-	message := "<b>Showing recent inspection (max 5) created by "+ user.Name+" in last 30 days</b>"
+	message := "<b>Showing recent inspections (max 5) created by "+ user.Name+" in last 30 days</b>"
 	message += "\n"
-	var url string
 	for _, inspection := range inspectionsRec {
-		url = "https://manage.happyco.com/folder/" + inspection.FolderID + "/inspections/" + inspection.ID
+		var url = "https://manage.happyco.com/folder/" + inspection.FolderID + "/inspections/" + inspection.ID
 		var date, _ = time.Parse(time.RFC3339, inspection.CreatedAt)
 		formattedDate := date.Format("02 Jan 2006 3:04PM")
 
