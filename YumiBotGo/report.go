01dab9262f6ad54ca89f3e232fbb7ef096847a57
Rohit Kumar <rohit@happy.co>
2018-08-25 14:31:35 +0930

    Adding report commmand logic :relaxed:

--- YumiBotGo/report.go
+++ YumiBotGo/report.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"time"
+)
+func init() {
+	commands["report"] = showRecentReports
+}
+//********************************************Adding commands********************************************
+func showRecentReports(user User, conversationID string, params ...string) {
+
+	reportsRec := getReports(user.UserID)
+	message := "<b>Showing recent reports (max 5) created by "+ user.Name+" in last 30 days</b>"
+	message += "\n"
+	for _, report := range reportsRec {
+		var url = "https://manage.happyco.com/reports/" + report.PublicID
+		var date, _ = time.Parse(time.RFC3339, report.CreatedAt)
+		var formattedDate = date.Format("02 Jan 2006 3:04PM")
+
+		message += "<a href=" + url + ">" + report.Name + "</a>" + " " + formattedDate
+		message += "\n"
+	}
+	addNote(conversationID, message)
+}
