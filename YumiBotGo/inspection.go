cfd9bead4466af1f12cd0053396c68359ee17a63
Rohit Kumar <rohit@happy.co>
2018-08-25 12:50:01 +0930

    Adding logic in inspection.go :lollipop:

--- YumiBotGo/inspection.go
+++ YumiBotGo/inspection.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	// "strings"
+	"time"
+	//"fmt"
+)
+func init() {
+	commands["inspection"] = showRecentInspections
+}
+//********************************************Adding commands********************************************
+func showRecentInspections(user User, conversationID string, params ...string) {
+
+	inspectionsRec := getInspections(user.UserID)
+	message := "<b>Showing recent inspection (max 5) created by "+ user.Name+" in last 30 days</b>"
+	message += "\n"
+	var url string
+	for _, inspection := range inspectionsRec {
+		url = "https://manage.happyco.com/folder/" + inspection.FolderID + "/inspections/" + inspection.ID
+		var date, _ = time.Parse(time.RFC3339, inspection.CreatedAt)
+		formattedDate := date.Format("02 Jan 2006 3:04PM")
+
+		message += "<a href=" + url + ">" + url + "</a>" + " " + formattedDate
+		message += "\n"
+	}
+	addNote(conversationID, message)
+}
