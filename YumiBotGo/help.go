f7327daf9c480d51fb0f87d3ab5aa3d8d7fc52f1
Rohit Kumar <rohit@happy.co>
2018-08-29 08:17:41 +0930

    Spacing

--- YumiBotGo/help.go
+++ YumiBotGo/help.go
@@ -6,7 +6,6 @@ import (
 
 //********************************************Adding commands********************************************
 func listRunCommands(author string, conversationID string, params ...string) {
-
 	name := strings.Fields(author)
 
 	message := "Yo! " + name[0] + "\n \n <b>Try following commands</b>"
