eea688993434dc3fe4ff4ec9c06c74e1b6d55701
Rohit Kumar <rohit@happy.co>
2018-08-28 13:56:43 +0930

    AirtableLinkedin: Code added which visits the profile of first 3 users getting record from base(airtble) :wolf: :rice_cracker:

---
+++ Froso/Linkedin/airtableLinkedin.js
@@ -0,0 +1,72 @@
+const puppeteer = require('puppeteer');
+const CREDS = require('./creds');
+
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'keyrYSQEONtptwMth'}).base('appfcatXnrEsiTmFB');
+
+base('scraped_data').select({
+    // Selecting the first 3 records in Grid view:
+    maxRecords: 3,
+    view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('linkedin_url'));
+        (async () => {
+            const browser = await puppeteer.launch({
+                headless: false
+            });
+
+          const page = await browser.newPage();
+
+          await page.goto("https://www.linkedin.com");
+
+          const USERNAME_SELECTOR = '#login-email';
+          const PASSWORD_SELECTOR = '#login-password';
+          const BUTTON_SELECTOR = '#login-submit';
+
+          await page.click(USERNAME_SELECTOR);
+          await page.keyboard.type(CREDS.username);
+
+          await page.click(PASSWORD_SELECTOR);
+          await page.keyboard.type(CREDS.password);
+          
+          await page.click(BUTTON_SELECTOR);
+          await page.waitForNavigation();
+            await page.goto(record.get('linkedin_url'));
+            var name = await page.evaluate(() => document.querySelector('div.pv-top-card-v2-section__info.mr5 > div.display-flex.align-items-center > h1').textContent)
+            console.log("The person's who profile you visited is:"+ name)
+            await browser.close();
+        })();
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});
+
+
+/*base('scraped_data').find('recz7ddYYhNCqUL1T', function(err, record) {
+    if (err) { console.error(err); return; }
+    // console.log(record);
+    console.log("Linkedin: "+record.fields.linkedin_url);
+
+});*/
+/*OUTPUT
+{
+    "id": "recz7ddYYhNCqUL1T",
+    "fields": {
+        "email": "nostensen@olenproperties.com",
+        "full_name": "Natalia Ostensen",
+        "last_title": "Owner",
+        "last_company": "Olen Properties",
+        "salesforce_id": "0034A00002kpY1fQAE",
+        "linkedin_url": "https://www.linkedin.com/in/natalia-ostensen-2818a61a"
+    },
+    "createdTime": "2018-08-23T17:33:15.000Z"
+}*/
