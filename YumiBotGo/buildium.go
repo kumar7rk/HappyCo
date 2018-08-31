889860a1293f500d4d01e8889475c4f748e7fa28
Rohit Kumar <rohit@happy.co>
2018-08-31 14:23:49 +0930

    Buildium adding logic for snoozeDays :cool:

--- YumiBotGo/buildium.go
+++ YumiBotGo/buildium.go
@@ -16,12 +16,21 @@ func sendBuildiumReply(user User, conversationID string, params ...string) {
 		params = strings.Split(user.Name, " ")
 	}
 	name := "there"
+	snoozeDays := int64(3)
+
 	if len(params) > 0 {
-		name = params[0]
+		val, err := strconv.Atoi(params[0])
+		if  err !=nil {
+			name = params[0]
+        } else{
+			name = strings.Split(user.Name, " ")[0]
+			snoozeDays = int64(val)
+        }
 	}
+	
 	message := "Hi " + name + " 👋 \n \n <b>Our friends at Buildium support your Happy Inspector subscription. \n\n They can be reached at 888-414-1988, or by submitting a ticket through your Buildium account.</b>\n\nBuildium Support team are the best place to help you with this query as they understand your unique workflow and are trained in Happy Inspector 💫 \n \n Please also feel free to take a look through our FAQ on the Buildium integration:  \n https://intercom.help/happyco/frequently-asked-questions/buildium-integration-faq/faq-buildium-integration  \n Thanks!  \n HappyBot ☺"
 
 	addReply(conversationID, message)
 	assignConversation(conversationID, "1615207")
-	snoozeConversation(conversationID)
+	snoozeConversation(conversationID, int64(3))
 }
