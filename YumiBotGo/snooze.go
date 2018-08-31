da98c7119134cbcb89578c2a468efdeddaf0fcf9
Rohit Kumar <rohit@happy.co>
2018-08-31 13:19:42 +0930

    Adding :sleepy: .go

---
+++ YumiBotGo/snooze.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"time"
+    "os"
+)
+
+func snoozeConversation(conversationID string, params ...string) {
+    days:=int64(3)
+    if len(params) > 0 {
+        days, _ = strconv.ParseInt(params[0], 10, 64)
+    }
+
+	currentTimeInSec := int64(time.Now().Unix())
+	secInADay := int64(86400)
+	snoozeTimeInSec := int64(secInADay*days)
+
+	snooze_until := currentTimeInSec + snoozeTimeInSec
+
+	url := "https://api.intercom.io/conversations/" + conversationID + "/reply"
+	payload := []byte(`{ "admin_id":"207278", "message_type":"snoozed", "snoozed_until":` + strconv.FormatInt(snooze_until, 10) + `}`)
+
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+	req.Header.Set("Authorization", "Bearer "+ os.Getenv("INTERCOM_ACCESS_TOKEN"))
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, _ := client.Do(req)
+
+    //MT- not sure about error handling here 
+	/*if err != nil {
+        fmt.Fprintf(os.Stderr, "Error in Snoozing conversation %v: %v\n", ID, err)
+    }*/
+    
+    defer resp.Body.Close()
+	
+    _, _ = ioutil.ReadAll(resp.Body)
+}
