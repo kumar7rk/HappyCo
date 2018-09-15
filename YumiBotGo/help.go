84c0d84b0f734f00ed720e6980a8036c823e2946
Rohit Kumar <rohit@happy.co>
2018-09-15 16:29:57 +0930

    Adding info about cmds in help

--- YumiBotGo/help.go
+++ YumiBotGo/help.go
@@ -9,6 +9,7 @@ func listRunCommands(author string, conversationID string, params ...string) {
 	name := strings.Fields(author)
 
 	message := "Yo! " + name[0] + "\n \n <b>Try following commands</b>"
+	message += "\n\n rep-reply to customer \n get- add a note"
 	for cmd, detail := range repCommands {
 			message += "<b>\n\n yumi rep " + cmd + "</b>\n\n" + detail.Description
 	}
