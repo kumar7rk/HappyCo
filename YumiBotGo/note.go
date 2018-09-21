2fd75691250690744172316f75320177cdf97b3d
Rohit Kumar <rohit@happy.co>
2018-09-21 11:23:07 +0930

    Updating note message for mrr and support level

--- YumiBotGo/note.go
+++ YumiBotGo/note.go
@@ -79,8 +79,8 @@ func makeNote(us_id string) string {
 		note += "<b>BusinessID:</b>" + business.ID + "\n"
 		note += "<b>Permissions:</b>" + BusinessPermission + "\n"
 		note += "<b>Role:</b>" + roles[permission] + "\n"
-		note += "<b>MRRID:</b>" + business.MRR+ "\n"
-		note += "<b><h3>Support Level:</b>" + business.SupportLevel + "<h3>\n"
+		note += "<b>MRRID:</b>" + business.MRR.String+ "\n"
+		note += "<b><h3>Support Level:</b>" + business.SupportLevel.String + "<h3>\n"
 	}
 	//******************constructing plan type string******************
 	for _, plan := range planTypeRec {
