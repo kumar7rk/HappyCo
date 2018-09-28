7900e30c1bf87f9c4db55a33e5349fffe17a97e5
Rohit Kumar <rohit@happy.co>
2018-09-28 10:48:43 +0930

    Using new message func for Buildium

--- YumiBotGo/buildium.go
+++ YumiBotGo/buildium.go
@@ -43,8 +43,8 @@ func sendBuildiumReply(user User, conversationID string, params ...string) {
 		}
 	}
 
-	message := "Hi " + name + " 👋 \n \n <b>Our friends at Buildium support your Happy Inspector subscription. \n\n They can be reached at 888-414-1988, or by submitting a ticket through your Buildium account.</b>\n\nBuildium Support team are the best place to help you with this query as they understand your unique workflow and are trained in Happy Inspector 💫 \n \n Please also feel free to take a look through our FAQ on the Buildium integration:  \n https://intercom.help/happyco/frequently-asked-questions/buildium-integration-faq/faq-buildium-integration  \n Thanks!  \n HappyBot ☺"
-
+	message :=buildiumMessage(name)
+	
 	addReply(conversationID, message)
 	assignConversation(conversationID, "1615207")
 	snoozeConversation(conversationID, snoozeDuration)
