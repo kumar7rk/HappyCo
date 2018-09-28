f05d4c2f18dd1a56fb2731c8ce95e456a9b4d1dc
Rohit Kumar <rohit@happy.co>
2018-09-28 13:40:29 +0930

    Adding return in all the funcs

--- YumiBotGo/constructMessages.go
+++ YumiBotGo/constructMessages.go
@@ -2,16 +2,20 @@ package main
 
 func buildiumMessage(name string) (message string) {
 	message = "Hi " + name + " 👋 \n \n <b>Our friends at Buildium support your Happy Inspector subscription. \n\n They can be reached at 888-414-1988, or by submitting a ticket through your Buildium account.</b>\n\nBuildium Support team are the best place to help you with this query as they understand your unique workflow and are trained in Happy Inspector 💫 \n \n Please also feel free to take a look through our FAQ on the Buildium integration:  \n https://intercom.help/happyco/frequently-asked-questions/buildium-integration-faq/faq-buildium-integration  \n Thanks!  \n HappyBot ☺"
+	return
 }
 
 func passwordMessage(name string) (message string) {
 	message = "Hi " + name + " 👋 \n \n It looks like you might be having trouble logging in? \n\n You can reset your password by entering your email <a href='https://manage.happyco.com/password/forgot'> here </a> \n \n Thanks!  \n HappyBot ☺ \n\n <i>Need to contact a human....... just reply</i>"
+	return
 }
 
 func followUpMessage(name string, authorName string) (message string) {
 	message = "Hey " + name + " 👋 \n \n Just wanted to message and check in, and see how you're going? \n\n Did our last message help? \n \n Shout out if you need any help.\n" + authorName + " 😄 "
+	return
 }
 
-func closingMessage(name string) (message string) {
+func closingMessage(name string, authorName string) (message string) {
 	message = "Hey " + name + " 👋 \n \n We're still here if you need any help. \n\n I'm closing this conversation for now, but you can reopen it at anytime by replying. \n \n We value your feedback Rohit- please rate us on AppStore: https://hpy.io/review 💯  \n" + authorName + " 😄 \n\n Get real-time intelligence on property conditions and portfolio trends to optimize operations, achieve higher NOI, and make better business decisions: http://hpy.io/happy-insights"
+	return
 }
