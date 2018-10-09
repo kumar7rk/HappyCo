cc83c01f3b9f9db6c9642e74484fa9b92e9109ef
Rohit Kumar <rohit@happy.co>
2018-10-09 17:27:25 +1030

    Refining follow up command logic :cool:

--- YumiBotGo/follow.go
+++ YumiBotGo/follow.go
@@ -1,48 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
 	"strings"
 	"time"
 )
 
 //********************************************Init********************************************
 func init() {
-	repCommands["follow"] = Command{Func:followUpConversation , Description: `Follow up message is sent. Conversation is snoozed for 3 days. After 3 days a closing message is sent
-		Want to cancel? Just enter a note during this time`}
+	repCommands["follow"] = RepCommand{Func:followUpConversation , Description: `Follow up message is sent. Conversation is snoozed for 5 days. After 5 days a closing message is sent
+	<b>Arguments</b> name
+	<b>Want to cancel? You can't. JK. Just enter a note during this time</b>
+	It will also cancel if customer messages during the snoozed duration`}
 }
 
 //********************************************Sending Buildium reply********************************************
-func followUpConversation(user User, conversationID string, params ...string) {
-	name := "there"
-	snoozeDuration := 7 * 24 * time.Hour
+func followUpConversation(user User, author Author, conversationID string, params ...string) {
+	userName := "there"
 
 	if user.Email != "" {
-		name = strings.Split(user.Name, " ")[0]
+		userName = strings.Split(user.Name, " ")[0]
 	}
-	if len(params) == 2 {
-		//assuming first param is a name
-		name = params[0]
-		//checking if the second param is a number or not
-		val, err := strconv.Atoi(params[1])
-		if err != nil {
-			fmt.Printf("Wrong parameters: %v\n", err)
-		} else {
-			snoozeDuration = time.Duration(val) * 24 * time.Hour
-		}
-	}
-	if len(params) == 1 {
-		val, err := strconv.Atoi(params[0])
-		if err != nil {
-			name = params[0]
-		} else {
-			snoozeDuration = time.Duration(val) * 24 * time.Hour
-		}
-	}
-
-	message:=followUpMessage(name,author)
-	addReply(conversationID, message)
-	assignConversation(conversationID, "1615207")
-	snoozeConversation(conversationID, snoozeDuration)
+	
+	message:=followUpMessage(userName,author.Name)
+	addReply(author.ID, conversationID, message)
+	addNote(conversationID,"Follow up sent")
+	snoozeConversation(conversationID, 3 * 24 * time.Hour)
 }
