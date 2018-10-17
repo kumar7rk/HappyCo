e8030b3ac821793e76a8df75329463295649bd1c
Rohit Kumar <rohit@happy.co>
2018-10-17 14:42:20 +1030

    Adding and implementing hasValidName in password

--- YumiBotGo/password.go
+++ YumiBotGo/password.go
@@ -21,13 +21,12 @@ func init() {
 func sendPasswordReply(user User, author Author, conversationID string, params ...string) {
 	name := "there"
 	snoozeDuration := 3 * 24 * time.Hour
-
-	if user.Email != "" {
+	if hasValidName(user) {
 		name = strings.Split(user.Name, " ")[0]
 	}
 	if len(params) == 2 {
 		//assuming first param is a name
-		name = params[0]
+		name = strings.Title(params[0])
 		//checking if the second param is a number or not
 		val, err := strconv.Atoi(params[1])
 		if err != nil {
@@ -39,7 +38,7 @@ func sendPasswordReply(user User, author Author, conversationID string, params .
 	if len(params) == 1 {
 		val, err := strconv.Atoi(params[0])
 		if err != nil {
-			name = params[0]
+			name = strings.Title(params[0])
 		} else {
 			snoozeDuration = time.Duration(val) * 24 * time.Hour
 		}
@@ -50,4 +49,32 @@ func sendPasswordReply(user User, author Author, conversationID string, params .
 	addReply(YumiBot.ID, conversationID, message)
 	assignConversation(conversationID, P3BoxID)
 	snoozeConversation(conversationID, snoozeDuration)
+}
+
+func hasValidName(user User) bool {
+	
+	firstName := strings.Split(user.Name, " ")[0]
+	userNameInLowerCase := strings.ToLower(user.Name)
+	
+	//checking certain keywords
+	var excludeList = []string {"property","inspector","management","maintenance","department","mgmt","dept"}
+	for _, name := range excludeList {
+		if strings.Contains(userNameInLowerCase, name) {
+			return false
+		}
+	}
+	//checking numbers
+	for i := 0; i < 9; i++ {
+		if strings.Contains(user.Name, strconv.Itoa(i)) {
+			return false
+		}
+	}
+	//checking acronyms
+	if firstName == strings.ToUpper(firstName) {
+	 	return false
+	}
+	if user.Email == "" {
+		return false
+	}
+	return true
 }
