f034c8ccd2491a440ce21bcefaf971d885ae5e04
Rohit Kumar <rohit@happy.co>
2018-10-20 12:08:37 +1030

    Complete follow.go with followUpProcess ready to :ship:

--- YumiBotGo/follow.go
+++ YumiBotGo/follow.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"strings"
 	"time"
 )
 
 //********************************************Init********************************************
 func init() {
 	repCommands["follow"] = RepCommand{Func: followUpConversation, Description: `A conversation is snoozed for 3 days. 
-	After 2 days a follow up message is sent from you. (a note "follow up sent" is added)
+	After 2 days a follow up message is sent from you. (a note "followed up" is added)
 	Again, the conversation is snoozed for a week.
 	After 4 days a closing message is sent.
 	<b>Want to cancel? You can't. JK. Just enter a note during this time</b>
@@ -17,14 +16,52 @@ func init() {
 
 //********************************************Follow up********************************************
 func followUpConversation(user User, author Author, conversationID string, params ...string) {
-	userName := "there"
-
-	if user.Email != "" {
-		userName = strings.Split(user.Name, " ")[0]
-	}
-
-	message := followUpMessage(userName, author.Name)
-	addReply(author.ID, conversationID, message)
-	addNote(conversationID, "Follow up sent")
 	snoozeConversation(conversationID, 3*24*time.Hour)
+}
+
+func followUpProcess() {
+	allOpenedConversations := listSnoozedConversations()
+
+	for _, convo := range allOpenedConversations {
+		notes := convo.ConversationParts.Parts
+		lastNote = notes[len(notes)-1]
+
+		m := make(map[string]string)
+		m["1544605"] = "Rohit"
+		m["424979"] = "Richelle"
+		m["2687597"] = "Anna"
+
+		followUpDuration := 2 * 24 * time.Hour
+		closeDuration := 4 * 24 * time.Hour
+		
+		noteAddedTime := lastNote.CreatedAt
+		followUpTime := time.Now().Add(-followUpDuration).Unix()
+		closeTime := time.Now().Add(-closeDuration).Unix()
+		
+		canFollowUp := false
+		if followUpTime > noteAddedTime {
+			canFollowUp = true
+		}
+
+		canClose := false
+		if closeTime > noteAddedTime {
+			canClose = true
+		}
+
+		authorID := lastNote.Author.ID
+		authorName := m[authorID]
+		userName := getUserName(convo.User.ID)
+
+		//checking if a conversation meets the parameters for following up
+		if lastNote.PartType == "note" && lastNote.Body == "<p>yumi rep follow</p>" && canFollowUp {
+			addReply(authorID, convo.ID, followUpMessage(userName, authorName))
+			snoozeConversation(convo.ID, 7*24*time.Hour)
+			addNote(convo.ID, "followed up")
+		}
+		//check if conversation can be closed
+		if lastNote.PartType == "note" && lastNote.Body == "<p>followed up</p>" && canClose {
+			addReply(authorID, convo.ID, closingMessage(userName, authorName))
+			addNote(convo.ID, "Conversation closed")
+		}
+	}
 }
