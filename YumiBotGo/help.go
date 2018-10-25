370141e26a2b4b04046b26b22f7fb18aaba654e2
Rohit Kumar <rohit@happy.co>
2018-10-26 08:11:57 +1030

    Rep to reply in help.go

--- YumiBotGo/help.go
+++ YumiBotGo/help.go
@@ -11,11 +11,11 @@ func listRunCommands(author string, conversationID string, params ...string) {
 	var salution = []string{"Howdy", "Namaste", "Yo", "Aloha", "Ni Hau", "Star lord", "Happy", "Bonjour", "Hola", "Hallo"}
 
 	message := "<h2>" + salution[rand.Intn(len(salution))] + " " + name[0] + "</h2>\n \n <b>Try following commands</b>"
-	message += "\n\n rep-reply to customer \n get- add a note"
+	message += "\n\n reply-reply to a customer \n get- add an internal/team note"
 	message += "\n\n <i>Arguments are optional</i>"
 
-	for cmd, detail := range repCommands {
-		message += "<b>\n\n yumi rep " + cmd + "</b>\n\n" + detail.Description
+	for cmd, detail := range replyCommands {
+		message += "<b>\n\n yumi reply " + cmd + "</b>\n\n" + detail.Description
 	}
 
 	for cmd, detail := range getCommands {
